e10aeae2b4ae7ed9501ba80417e31b8839945860
Naho <maranaho_nguessan@intuit.com>
2024-02-22 01:46:22 -0800

    testing assets

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,10 +4,16 @@ const Movie = ({movieKey}) => {
     const title = movies[movieKey]
     const thumb = files[`${movieKey}Thumb`]
     const screen = files[`${movieKey}Screen`]
+    const video = files[movieKey]
     return (
         <article>
             <h2>{title}</h2>
-            <img src={thumb}/>
+            <img src={thumb} width="300"/>
+            <img src={screen} width="300"/>
+            <video
+                src={video}
+                width="300"
+            />
         </article>
     )
 }
